1da015aa0379ecc291f68dd719411f29641e0208
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-07-16 09:38:01 -0400

    in progress

---
+++ config/orm.js
@@ -0,0 +1,62 @@
+// selectAll()
+// insertOne()
+// updateOne()
+
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+// Object for all our SQL statement functions.
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols;
+    queryString += ") ";
+    queryString += "VALUES (?)";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objColVals;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+
+};
+
+// Export the orm object for the model (buger_controller.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Jr. Bacon Cheeseburger"), ("Texas Style Cheeseburger"), ("The Big Cheese");
